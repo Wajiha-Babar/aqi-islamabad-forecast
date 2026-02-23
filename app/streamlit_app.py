2e60d50f6ecb84c44c2fc3c418ca56c83862e69d
Wajiha-Babar <thaheemwajeeha@gmail.com>
2026-02-23 11:27:27 +0500

    update history streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -284,9 +284,7 @@ def load_history_last_7_days():
     project = get_hw_project_safe()
     fs = project.get_feature_store()
     fg = fs.get_feature_group(FEATURE_GROUP_NAME, version=FEATURE_GROUP_VERSION)
-   
-        h = fg.read()
-
+    h = fg.read()
     h.columns = [c.lower() for c in h.columns]
     if "timestamp_utc" not in h.columns:
         return pd.DataFrame()
